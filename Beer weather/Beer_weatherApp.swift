a80ec139fdc9813701a46baef3593f2b81520e2d
Bjørn Eide <69849779+h595855@users.noreply.github.com>
2026-05-20 20:53:57 +0200

    Initial Commit

---
+++ Beer weather/Beer_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beer_weatherApp.swift
+//  Beer weather
+//
+//  Created by Bjørn Eide on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Beer_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beer weatherTests/Beer_weatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Beer_weatherTests.swift
+//  Beer weatherTests
+//
+//  Created by Bjørn Eide on 20/05/2026.
+//
+
+import Testing
+@testable import Beer_weather
+
+struct Beer_weatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
